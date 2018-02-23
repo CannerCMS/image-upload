81bc53a1e975c7e200e2adfd6f097fff876b50bc
chilijung <chilijung@gmail.com>
2018-02-23 18:27:26 +0800

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-  extends: [
-    "google",
+  "extends": [
+    "prettier",
     "plugin:react/recommended"
   ],
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaVersion: 7,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
     }
   },
-  env: {
-    browser: true
+  "env": {
+    "browser": true
   },
-  plugins: [
+  "plugins": [
+    "prettier",
     "react"
   ],
-  settings: {
-    react: {
-      pragma: "React",  // Pragma to use, default to "React"
-      version: "15.3.1"
+  "settings": {
+    "react": {
+      "pragma": "React",  // Pragma to use, default to "React"
+      "version": "15.3.1"
     }
   },
   "rules": {
